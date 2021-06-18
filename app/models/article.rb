d7fdc85c335ac1cc244c93a273600d8a4c8879b8
Romuald Shmidtelson <d-w-b@mail.ru>
2021-06-18 16:35:47 +0300

    Implemented article.recent scope and testing it

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   validates :slug, uniqueness: true, presence: true
+
+  scope :recent, -> { order(created_at: :desc)}
 end
