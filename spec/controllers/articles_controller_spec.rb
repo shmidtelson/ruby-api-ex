48d9640f7cb6acf68a67a93a3a59c27e2ccec35a
Romuald Shmidtelson <d-w-b@mail.ru>
2021-06-18 16:16:45 +0300

    Updated controller spec tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe "#index" do
+    subject { get :index}
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status :ok
     end
 
     it 'should return proper json' do
-      create_list :article, 2
-      get :index
-      json = JSON.parse(response.body)
-      json_data = json['data']
+      articles = create_list :article, 2
+      subject
+
       expect(json_data.length).to eq(2)
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+                                                       'title' => article.title,
+                                                       'content' => article.content,
+                                                       'slug' => article.slug
+                                                     })
+      end
     end
   end
 end
